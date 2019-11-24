6e99dbde51c7b210d19588c382373e0d943ffa8e
jcc1002 <44771909+jcc1002@users.noreply.github.com>
2019-11-24 21:27:54 +0100

    cam

--- jugar.sh
+++ jugar.sh
@@ -1,2 +1,2 @@
-/usr/bin/python3 /home/jose/Desktop/practica-dms-2019-2020-master/src/components/client/test_connection.py
-/usr/bin/python3 /home/jose/Desktop/practica-dms-2019-2020-master/src/components/client/main.py
+/usr/bin/python3 ./src/components/client/test_connection.py
+/usr/bin/python3 ./src/components/client/main.py
