df64752dcd4e988f13fffb79fa8da4fba1c66356
jcc1002 <44771909+jcc1002@users.noreply.github.com>
2019-11-24 21:08:44 +0100

    Implementación del script jugar.sh

---
+++ jugar.sh
@@ -0,0 +1,2 @@
+/usr/bin/python3 /home/jose/Desktop/practica-dms-2019-2020-master/src/components/client/test_connection.py
+/usr/bin/python3 /home/jose/Desktop/practica-dms-2019-2020-master/src/components/client/main.py
