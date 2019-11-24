0e21c2818d195d2c2b7e75e3e805d2c03804615f
ars1004 <ars1004@alu.ubu.es>
2019-11-24 15:27:54 +0100

    Movido cliente de la carpeta server a client

--- src/components/client/test_connection.py
+++ src/components/client/test_connection.py
@@ -1,18 +1,18 @@
 import requests
 
-ip = '172.10.1.10'
-puerto = '1234'
-
+ip = '172.10.1.20'
+puerto = '4567'
+print('Obtener')
 r = requests.get('http://'+ip+':'+puerto)
 print(r.text)
 
+name = 'servidor tanwsud tan chulo'
+host = '172.10.1.30'
+port = '9876'
 
-usuario = 'asdfasdfas'
-password = '12341234'
+c = requests.post('http://'+ip+':'+puerto+'/server/register', data = {'name': name, 'host': host, 'port': port})
 
-c = requests.post('http://'+ip+':'+puerto+'/user/create', data = {'username': usuario, 'password': password})
 
-c = requests.post('http://'+ip+':'+puerto+'/user/login', data = {'username': usuario, 'password': password})
-
+c = requests.get('http://'+ip+':'+puerto+'/server', data = {'token': token})
 print(c)
 print(c.text)
