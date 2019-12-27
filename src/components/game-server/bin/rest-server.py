f7f958e0f873edc395daf4c85882d7b1418ea1dc
ars1004 <ars1004@alu.ubu.es>
2019-12-27 17:58:56 +0100

    Cambiada documentación del servidor

--- src/components/game-server/bin/rest-server.py
+++ src/components/game-server/bin/rest-server.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def status():
-    """ Status handler. Useful to test whether the server is running or not.
+    """ Endpoint de verificación de estado. 
+    Util para comprobar la conexión con el servidor.
     ---
     get:
-        summary: Status handler.
-        description: Status testing endpoing.
-        responses:
+        resumen: Verificación de estado.
+        respuestas:
             200:
-                description: The server is running correctly.
+                descripción: El servidor se está ejecutando correctamente.
     """
     (code, message) = rest_api.status(request)
     if (code == 200):
@@ -29,18 +29,18 @@ def unirse():
     """ Endpoint de conexion de usuarios.
     ---
     post:
-        summary: Usuario se une al servidor.
-        description: El endpoint se usa para que un usuario se conecte al servidor.
-        parameters:
+        resumen: Usuario se une al servidor.
+        descripción: El endpoint se usa para que un usuario se conecte al servidor.
+        parámetros:
             - token: Token del usuario.
             - nombre: (opcional) Nombre del usuario en la partida.
-        responses:
+        respuestas:
             200:
-                description: El usuario se ha unido correctamente.
+                descripción: El usuario se ha unido correctamente.
             401:
-                description: El token del usuario no se ha podido verificar.
+                descripción: El token del usuario no se ha podido verificar.
             500:
-                description: El usuario no se ha podido unir.
+                descripción: El usuario no se ha podido unir.
     """
     (code, message) = rest_api.unirse(request)
     if (code == 200):
@@ -53,17 +53,17 @@ def dar_de_alta():
     """ Endpoint de registro de servidor.
     ---
     post:
-        summary: Registra el servidor en el hub.
-        parameters:
+        resumen: Registra el servidor en el hub.
+        parámetros:
             - nombre: (opcional) Nombre del servidor.
             - token: token de usuario
-        responses:
+        respuestas:
             200:
-                description: El servidor se ha registrado correctamente.
+                descripción: El servidor se ha registrado correctamente.
             401:
-                description: El token del usuario no se ha podido verificar.
+                descripción: El token del usuario no se ha podido verificar.
             500:
-                description: El servidor no se ha podido registrar.
+                descripción: El servidor no se ha podido registrar.
     """
     (code, message) = rest_api.dar_de_alta(request)
     if (code == 200):
@@ -76,17 +76,17 @@ def dar_de_baja():
     """ Endpoint de borrado de servidor.
     ---
     post:
-        summary: Borra el servidor del hub.
-        parameters:
+        resumen: Borra el servidor del hub.
+        parámetros:
             - name: (opcional) Nombre del servidor.
             - token: token de usuario
-        responses:
+        respuestas:
             200:
-                description: El servidor se ha borrado correctamente.
+                descripción: El servidor se ha borrado correctamente.
             401:
-                description: El token del usuario no se ha podido verificar.
+                descripción: El token del usuario no se ha podido verificar.
             500:
-                description: El servidor no se ha podido borrar.
+                descripción: El servidor no se ha podido borrar.
     """
     (code, message) = rest_api.dar_de_baja(request)
     if (code == 200):
@@ -99,13 +99,13 @@ def obtener_estado():
     """ Endpoint de obtencion del estado de la partida.
     ---
     post:
-        summary: Obtiene el estado de la partida.
-        responses:
+        resumen: Obtiene el estado de la partida.
+        respuestas:
             200:
-                description: Se ha obtenido el estado. El contenido de la respuesta
+                descripción: Se ha obtenido el estado. El contenido de la respuesta
                     contiene el estado en formato JSON.
             500:
-                description: No se ha podido obtener el estado de la partida.
+                descripción: No se ha podido obtener el estado de la partida.
     """
     (code, message) = rest_api.obtener_estado(request)
     if (code == 200):
@@ -118,13 +118,13 @@ def obtener_juegos():
     """ Endpoint de obtencion de los juegos que dispone el servidor.
     ---
     post:
-        summary: Obtiene los juegos que tiene el servidor.
-        responses:
+        resumen: Obtiene los juegos que tiene el servidor.
+        respuestas:
             200:
-                description: Se han obtenido los juegos. El contenido de la respuesta
+                descripción: Se han obtenido los juegos. El contenido de la respuesta
                     contiene los juegos en formato JSON.
             500:
-                description: No se han podido obtener los juegos.
+                descripción: No se han podido obtener los juegos.
     """
     (code, message) = rest_api.obtener_juegos(request)
     if code == 200:
@@ -137,17 +137,17 @@ def seleccionar_juego():
     """ Endpoint de seleccion de juego.
     ---
     post:
-        summary: Selecciona el juego que se va a jugar en el servidor.
-        parameters:
+        resumen: Selecciona el juego que se va a jugar en el servidor.
+        parámetros:
             - token: Token del usuario.
             - juego: Juego que se quiere seleccionar.
-        responses:
+        respuestas:
             200:
-                description: El juego se ha seleccionado correctamente.
+                descripción: El juego se ha seleccionado correctamente.
             401:
-                description: El token del usuario no se ha podido verificar.
+                descripción: El token del usuario no se ha podido verificar.
             500:
-                description: El juego no se ha podido seleccionar.
+                descripción: El juego no se ha podido seleccionar.
     """
     (code, message) = rest_api.seleccionar_juego(request)
     if code == 200:
@@ -160,17 +160,17 @@ def hacer_movimiento():
     """ Endpoint de realizacion de movimiento.
     ---
     post:
-        summary: Hace un movimiento en el juego.
-        parameters:
+        resumen: Hace un movimiento en el juego.
+        parámetros:
             - token: Token del usuario.
             - movimiento: Movimiento del usuario.
-        responses:
+        respuestas:
             200:
-                description: El movimiento se ha realizado correctamente.
+                descripción: El movimiento se ha realizado correctamente.
             401:
-                description: El token del usuario no se ha podido verificar.
+                descripción: El token del usuario no se ha podido verificar.
             500:
-                description: El movimiento no se ha podido realizar.
+                descripción: El movimiento no se ha podido realizar.
     """
     (code, message) = rest_api.hacer_movimiento(request)
     if (code == 200):
--- src/components/game-server/lib/presentation/restapi.py
+++ src/components/game-server/lib/presentation/restapi.py
@@ -9,9 +9,9 @@ from lib.util.player_util import PlayerUtil
 import sys
 
 class RestApi():
-    """ REST API facade.
+    """ Fachada de la API REST.
     ---
-    This class is a facade with the operations provided through the REST API.
+    Esta clase es una fachada con las operaciones proporcionadas por la API REST.
     """
     def __init__(self):
         self.authCon = authServerCon()
@@ -21,24 +21,24 @@ class RestApi():
         # self.juego.check()
 
     def status(self, request):
-        """ Status handler.
+        """ Comprobar estado.
         ---
-        Always returns a tuple with the 200 status code and an "OK" message.
+        Siempre devuelve una tupla con el código de estado 200 y el mensaje "OK".
         """
         return (200, 'OK')
 
     def unirse(self, request):
-        """ Gestor de union de usuario.
+        """ Gestor de unión de usuario.
         ---
         Une un usuario a la partida.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, n) cuando se une con exito. n es el numero del jugador.
-                - (401, 'Unauthorized') cuando el token es erroneo
-                - (500, 'Server error') cuando ocurre un error.
+                - (401, 'Acceso denegado') cuando el token es erroneo
+                - (500, 'Error del servidor') cuando ocurre un error.
         """
         try:
             token = request.form['token']
@@ -51,21 +51,21 @@ class RestApi():
                 self.player_util.add_player(token, n)
                 return (200, str(n))
             else:
-                return (401, 'Unauthorized')
+                return (401, 'Acceso denegado')
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Server error')
+            return (500, 'Error del servidor')
 
     def dar_de_alta(self, request):
         """ Da de alta el servidor en el hub.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, 'OK') cuando se da de alta correctamente.
-                - (401, 'Unauthorized') cuando no se puede verificar al usuario.
-                - (500, 'Error) cuando falla el servidor.
+                - (401, 'Acceso denegado') cuando no se puede verificar al usuario.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
         try:
             token = request.form['token']
@@ -74,23 +74,23 @@ class RestApi():
                 if self.hubCon.darDeAlta():
                     return (200, 'OK')
                 else:
-                    return (500, 'Error')
+                    return (500, 'Error del servidor')
             else:
-                return (401, 'Unauthorized')
+                return (401, 'Acceso denegado')
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
 
     def dar_de_baja(self, request):
         """ Da de baja el servidor en el hub.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, 'OK') cuando se da de alta correctamente.
-                - (401, 'Unauthorized') cuando no se puede verificar al usuario.
-                - (500, 'Error) cuando falla el servidor.
+                - (401, 'Acceso denegado') cuando no se puede verificar al usuario.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
 
         try:
@@ -100,22 +100,22 @@ class RestApi():
                 if self.hubCon.darDeBaja():
                     return (200, 'OK')
                 else:
-                    return (500, 'Error')
+                    return (500, 'Error del servidor')
             else :
-                return (401, 'Unauthorized')
+                return (401, 'Acceso denegado')
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
 
     def obtener_estado(self, request):
         """ Obtiene el estado de una partida.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, estado) cuando se obtiene el estado correctamente.
-                - (500, 'Error) cuando falla el servidor.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
         try:
             estado = self.juego.obtenerEstado()
@@ -123,18 +123,18 @@ class RestApi():
             return (200, estado_json)
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
 
     def hacer_movimiento(self, request):
         """ Hace un movimiento.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, 'OK') cuando el movimiento se ha realizado correctamente.
-                - (401, 'Unauthorized') cuando no se puede verificar al usuario.
-                - (500, 'Error) cuando falla el servidor.
+                - (401, 'Acceso denegado') cuando no se puede verificar al usuario.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
         try:
             token = request.form['token']
@@ -146,22 +146,22 @@ class RestApi():
                 self.juego.movePiece(x, y, n)
                 return (200, 'OK')
             else:
-                return (401, 'Unauthorized')
+                return (401, 'Acceso denegado')
         except Exception as e: 
             print(e, file=sys.stderr)
             print(n, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
 
 
     def obtener_juegos(self, request):
         """ Obtiene un listado de los juegos disponibles.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, 'OK') cuando se obtienen correctamente los juegos.
-                - (500, 'Error) cuando falla el servidor.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
         try:
             #TODO obtener los juegos disponibles
@@ -169,18 +169,18 @@ class RestApi():
             return (200, 'OK')
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
 
     def seleccionar_juego(self, request):
         """ Selecciona el juego del servidor.
 
-        Parameters:
-            - request: The HTTP request received in the REST endpoint.
-        Returns:
-            A tuple with the following values:
+        parámetros:
+            - request: La petición HTTP recibida en el endpoint.
+        devuelve:
+            Una tupla con los siguientes valores:
                 - (200, 'OK') cuando el juego se ha seleccionado correctamente.
-                - (401, 'Unauthorized') cuando no se puede verificar al usuario.
-                - (500, 'Error) cuando falla el servidor.
+                - (401, 'Acceso denegado') cuando no se puede verificar al usuario.
+                - (500, 'Error del servidor') cuando falla el servidor.
         """
 
         try:
@@ -191,7 +191,7 @@ class RestApi():
 
                 return (200, 'OK')
             else:
-                return (401, 'Unauthorized')
+                return (401, 'Acceso denegado')
         except Exception as e: 
             print(e, file=sys.stderr)
-            return (500, 'Error')
+            return (500, 'Error del servidor')
